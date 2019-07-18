2b1b2c08801cbc050c19610325eccd27face8ac2
ireneterpstra <ireneterpstra2@gmail.com>
2019-07-18 10:44:34 -0400

    Running Motor Example

--- Tur6oTest/Tur6oTest.ino
+++ Tur6oTest/Tur6oTest.ino
@@ -4,12 +4,33 @@
  * 
  */
 
+int RMPin1 = 11;
+int RMPin2 = 10;
+int LMPin1 = 9;
+int LMPin2 = 3;
+
+int power;
+int motorPower = 15; 
+
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(RMPin1, OUTPUT);
+  pinMode(RMPin2, OUTPUT);  
+  pinMode(LMPin1, OUTPUT);
+  pinMode(LMPin2, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  power =  map(motorPower, -100, 100, 124, 253);
+  Serial.println("Power = " + String(power));
+  analogWrite(RMPin1, power);
+  analogWrite(RMPin2, power);
+  analogWrite(LMPin1, power);
+  analogWrite(LMPin2, power);
 
 }
